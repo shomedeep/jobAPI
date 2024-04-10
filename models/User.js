af86a46e19ab3d3d491a67dedf2fbee60ee67a2f
Shomedeep <shomedeepmondal@gmail.com>
2024-04-10 17:23:33 +0530

    some error handle defined

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, "PLease provide password"],
+    required: [true, "Please provide password"],
     minlength: 3,
   },
 });
