1d5c4c32becd317eebef722310dc26be139c6a58
Shomedeep <shomedeepmondal@gmail.com>
2024-04-10 18:47:55 +0530

    security middleware added

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 // async errors
 require("express-async-errors");
 
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -19,6 +25,19 @@ const errorMiddleware = require("./middlewares/error-handler");
 
 app.use(express.json());
 app.use(express.static("./public"));
+
+// security middle
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
 //route
 app.use("/api/v1", mainRouter);
